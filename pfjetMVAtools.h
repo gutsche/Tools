5b1ef8255e8ceb467f65e00465a2cdce5d7cdfbf
cwelke <>
2013-07-12 13:03:36 +0000

    fixed header

--- pfjetMVAtools.h
+++ pfjetMVAtools.h
@@ -3,6 +3,9 @@
 
 typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<float> > LorentzVector;
 
+#ifndef PFJETMVATOOLS_H
+#define PFJETMVATOOLS_H
 bool sortByPFJetPt (const LorentzVector &pfjet1, const LorentzVector & pfjet2);
 std::vector <Int_t> sortcorrectedjets (const std::vector <LorentzVector> & corrpfjets);
 bool getGoodMVAs(std::vector <float> &goodmvas);
+#endif
